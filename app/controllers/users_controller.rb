8e403ed3a832deb163c8e85af2153c6feecb50e8
sanchojuniorti <sanchojunior.ce@gmail.com>
2024-10-02 14:10:55 -0300

    feat: :sparkles: Add Actions Form Edit Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  require_relative '../services/github_scraper'  # Adjust path as necessary
+  require_relative '../services/github_scraper'
+  before_action :set_user_params, only: %i[destroy profile edit update]
 
   def index
     @q = User.ransack(params[:q])
@@ -10,24 +11,51 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def profile end
+
+  def edit end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      scraper = GithubScraper.new(@user.name)
-      github_data = scraper.scrape
-      @user.update(github_data)
+      github_scraper
       redirect_to users_path, notice: 'Usuário criado com sucesso.'
     else
       render :new
     end
   end
 
+  def update
+    binding.pry
+    if @user.update(user_params)
+      github_scraper
+      redirect_to users_path, notice: 'Usuário atualizado com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to users_path, notice: 'Usuário deletado com sucesso.' if @user.destroy
+  end
+
   private
+
+  def set_user_params
+    @user = User.find(params[:id]) if params[:id]
+  end
+
   def user_params
     params.require(:user).permit(:name, :address_web, 
       :github_username, :followers, :following,
       :stars, :contributions, :profile_image_url,
       :organization, :location
     ) if params[:user].present?
+  end
+
+  def github_scraper
+    scraper = GithubScraper.new(@user.name)
+    github_data = scraper.scrape
+    @user.update(github_data)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :users, only: [:new, :create, :index]
+  resources :users, only: [:new, :create, :index, :edit, :destroy, :update] do
+    get 'profile', on: :member
+  end
   
   #get 'users/:username', to: 'users#index', as: 'user'
   #get 'users/new', to: 'users#new', as: 'new'
